26d2f103bec39cd9e9081a120e470366d8981d8d
snow <xueyao_98@foxmail.com>
2017-11-11 17:26:21 +0800

    complement the return msg of web api

--- src/main/java/res/LogsRes.java
+++ src/main/java/res/LogsRes.java
@@ -2,8 +2,11 @@ package res;
 
 import com.codahale.metrics.annotation.Timed;
 import entity.Result;
-
+import entity.Status;
+import mongodb.dao.LogDao;
 import org.hibernate.validator.constraints.NotEmpty;
+import orm.Log;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
@@ -13,17 +16,36 @@ import javax.ws.rs.core.MediaType;
 @Path("/api/v1/logs")
 @Produces(MediaType.APPLICATION_JSON)
 public class LogsRes {
+    private LogDao logDao;
 
-    public LogsRes(){
-
+    public LogsRes() {
+        logDao = new LogDao();
     }
 
     @POST
     @Timed
     //创建一条新日志：需提供服务名称与日志内容
     public Result createLog(@NotEmpty @QueryParam("name") String name,
-                            @NotEmpty @QueryParam("content") String content){
-        return null;
+                            @NotEmpty @QueryParam("content") String content) {
+
+        //-----------解析Log的内容（content）是不是放在DAO层做比较好？-----------
+        Log log = new Log();
+        logDao.add(log);
+        //-----------------------------------------------------------------
+
+
+//        {
+//                "_id" : ObjectID("[特定值]"),
+//                "name" : "courseservice",
+//                "ip" : "123.207.73.150",
+//                "dateTime" : "04/Nov/2017:11:43:54",
+//                "url" : "GET /application/api/v2/class? HTTP/1.1",
+//                "status" : 200,
+//                "client" : "Apache-HttpClient/4.5.2 (Java/1.8.0_151)"
+//        }
+
+        //创建成功时：
+        return new Result("CREATED", Status.CREATED, "");
     }
 
     @GET
@@ -31,9 +53,17 @@ public class LogsRes {
     //查询日志：根据限制条件查询日志列表
     public Result queryLogs(@NotEmpty @QueryParam("name") String name,
                             @QueryParam("ip") String ip,
-                            @QueryParam("dateTime") String datetime,
-                            @QueryParam("client") String client){
-        return null;
+                            @QueryParam("fromDateTime") String fromDateTime,
+                            @QueryParam("toDateTime") String toDateTime,
+                            @QueryParam("client") String client) {
+
+        String data = logDao.queryByParam(name, ip, fromDateTime, toDateTime, client);
+        if (data == null) {
+            return new Result("NOT FOUND", Status.NOT_FOUND, "");
+        }
+
+        //----------后期：添加查询到的日志数量（参见courserservice中的Page），data的返回类型----------
+        return new Result("", Status.OK, data);
     }
 
     @DELETE
@@ -42,10 +72,18 @@ public class LogsRes {
     public Result deleteLogs(@NotEmpty @QueryParam("name") String name,
                              @QueryParam("ip") String ip,
                              @QueryParam("dateTime") String datetime,
-                             @QueryParam("client") String client){
-        return null;
+                             @QueryParam("client") String client) {
+
+        //----------------------------待完善----------------------------
+        //logDao.delete();
+        //-------------------------------------------------------------
+
+
+        //删除成功时：
+        return new Result("", Status.NO_CONTENT, "");
     }
 }
+
 
 /*
 （1）定义LogsRes、ErrorsRes中的所有方法 ok
